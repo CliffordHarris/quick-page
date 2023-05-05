79066fa48cd49cf12b1facc4a41ce720544d0492
Dan Yang <dan@Dans-MacBook-Pro.local>
2023-05-05 11:23:09 -0500

    Change word and add class func

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 "use strict";
 let textId = document.getElementById("text");
-
-function changeClass() {
-    textId.className = "text2";
+function changeText(start){
+    let text = "";
+    if (start === "Meh...") {
+        text = start = "Wow!!!";
+        changeClass("text1");
+    } else {
+        text = start = "Meh...";
+        changeClass("text1");
+    }
+    textId.innerHTML = text;
 }
-// setInterval(changeText, 5000);
+function changeClass(txt) {
+    textId.className = txt;
+}
